acdc0fcf7b394ae5c2d0b1892f2dc5ad90480701
KarlaHerrera15 <162621653+KarlaHerrera15@users.noreply.github.com>
2025-04-04 08:56:40 +0000

    punto 6

---
+++ angular-reddit/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ArticleComponent } from './article/article.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, ArticleComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    let t = title.value;
+    let v= link.value;
+    console.log(`Adding article title: `+t+ ` and link: `+ v);
+    console.log(`Adding article title: `+title.value+ ` and link: `+ link.value);
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+  return false;
+ }
+}
